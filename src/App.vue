47ce04314efbce3fcf73e5d14935e0892401973f
Daisuke Matsumoto <sagirikiri@gmail.com>
2019-03-21 02:29:24 +0900

    [fix]UIの修正

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,33 @@
 <template>
-  <v-app id="app">
-    <comHeader />
-    <router-view />
+<div id="app">
+  <v-app id="inspire">
+    <v-container fluid grid-list-lg>
+      <v-layout row wrap>
+        <v-flex xs12 sm12 md12>
+          <v-card>
+            <v-toolbar>
+              <v-toolbar-side-icon></v-toolbar-side-icon>
+            </v-toolbar>
+            <router-view />
+            <v-card-text style="height: 100px; position: relative">
+              <v-btn
+                absolute
+                dark
+                fab
+                top
+                right
+                color="pink"
+                v-on:click="callComponent(EditTask)"
+              >
+                <v-icon>add</v-icon>
+              </v-btn>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </v-app>
+</div>
 </template>
 
 <script>
@@ -23,6 +48,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/EditTask.vue
+++ src/components/EditTask.vue
@@ -1,3 +1,33 @@
 <template>
-    
+    <v-app>
+        <div>
+            <v-dialog
+                v-model="dialog">
+                <template v-slot:activator="{ on }">
+                    <v-btn
+                    v-on="on">
+                        新規登録
+                    </v-btn>
+                </template>
+                <v-card>
+                    <v-card-title>
+                        <v-input></v-input>
+                    </v-card-title>
+                    <v-card-text>
+
+                    </v-card-text>
+                </v-card>
+            </v-dialog>
+        </div>
+    </v-app>    
 </template>
+
+<script>
+export default {
+    data(){
+        return {
+            dialog: false
+        }
+    }
+}
+</script>
