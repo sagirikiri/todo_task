05d6b90895adb3b3c870898a25075872d97c465e
Daisuke Matsumoto <sagirikiri@gmail.com>
2019-03-28 21:49:36 +0900

    [fix]ボタンの削除

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,14 @@
 <template>
 <div id="app">
-  <v-app id="inspire">
-    <v-container fluid grid-list-lg>
-      <v-layout row wrap>
-        <v-flex xs12 sm12 md12>
-          <v-card>
-            <v-toolbar>
-              <v-toolbar-side-icon></v-toolbar-side-icon>
-            </v-toolbar>
-            <router-view />
-            <v-card-text style="height: 100px; position: relative">
-              <v-btn
-                absolute
-                dark
-                fab
-                top
-                right
-                color="pink"
-                v-on:click="callComponent(EditTask)"
-              >
-                <v-icon>add</v-icon>
-              </v-btn>
-            </v-card-text>
-          </v-card>
-        </v-flex>
+	<v-app id="inspire">
+	<v-container fluid grid-list-lg>
+		<v-layout row wrap>
+			<v-flex xs12 sm12 md12>
+					<v-toolbar>
+						<v-toolbar-side-icon></v-toolbar-side-icon>
+					</v-toolbar>
+				<router-view />
+    		 </v-flex>
       </v-layout>
     </v-container>
   </v-app>
@@ -32,11 +17,21 @@
 
 <script>
 import ComHeader from '@/components/ComHeader.vue'
+import EditTask from '@/components/EditTask.vue'
 
 export default {
   name: 'App',
   components: {
-    ComHeader
+    ComHeader,
+    EditTask
+  },
+  data(){
+	return {
+		dialog:false
+	}
+  },
+  methods: {
+
   }
 }
 </script>
